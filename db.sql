cf56e9c8ccc4a9bdd2c277d4b1ceaa24956122e4
Derek Leiva <dereklevilla45@gmail.com>
2025-05-26 09:27:04 -0600

    Creacion  de tabla categorias + recetas

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS bdPrueba;
 
-CREATE TABLE IF NOT EXISTS bdPrueba.users (
+CREATE TABLE IF NOT EXISTS avii.users (
     userId INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     profesion VARCHAR(100),
@@ -8,4 +8,24 @@ CREATE TABLE IF NOT EXISTS bdPrueba.users (
     edad INT,
     peso DECIMAL(5,2),
     estatura DECIMAL(4,2)
+);
+
+CREATE TABLE IF NOT EXISTS avii.categorias_recetas (
+    categoriaId INT AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(100) NOT NULL,
+    nombre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS avii.recetas (
+    recetaId INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion TEXT NOT NULL,
+    ingredientes TEXT NOT NULL,
+    instrucciones TEXT NOT NULL,,
+    tipo VARCHAR(50) NOT NULL,
+    categoriaId INT,
+    fechaCreacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    diaRecomendado VARCHAR(20),
+    calorias INT,
+    FOREIGN KEY (categoriaId) REFERENCES avii.categorias_recetas(categoriaId)
 );
